4deae3807e2f44288ebfb26e2efec86d9196a2b1
erwinfs <erwin@frank-schultz.com>
2021-05-20 17:45:27 +0100

    Update power_batch.py

--- power_batch.py
+++ power_batch.py
@@ -1,9 +1,12 @@
 # This program allows the user to select a subfolder from where it is run
-# and processes all .fit files that subfolders.
+# and processes all .fit files that subfolder.
 # The subfolder can have an optional race distance in km. This shold be added
 # following an underscore and should be the only underscore in the folder name
-# .fit files should be of the format ww-name-yyyy-mm-dd.fit
-# where ww is the weight in kg (could be www :-) and name is the name of rider
+# .fit files should be of the format name_ww_ftp_yyyy-mm-dd.fit
+# where name is the name of rider, ww is the weight in kg (could be www :-) and
+#  ftp is the ridder's current ftp. This is not very robust, no checking done on
+# file & folder name formats. Results with malformed names may be unpredictable.
+
 import csv
 import os
 import glob
@@ -13,6 +16,10 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from tkinter import *
+import pprint as pp
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
 
 # Set timezone
 UTC = pytz.UTC
@@ -64,6 +71,80 @@ def read_fit_file(fit_input, allowed_fields, required_fields):
             data.append(mdata)
     return data
 
+# Create and populate df fields for moving average for each power interval
+# and calculate maxima
+def power_interval_stats(df, weight, res_dict):
+    # Must include 30 (for NP) and 1200 (for FTP)
+    power_intervals = [15, 30, 60, 300, 1200]
+
+    power_max = [] # List to store max power for each time interval
+    for i in power_intervals:
+        field_name = 'power_ma' + str(i)
+        interval = str(i) + 's'
+        df[field_name] = df['power'].rolling(interval, min_periods = i).mean()
+        max_power = df[field_name].max()
+        power_max.append(max_power)
+
+        # Create Columns min and seconds and calculate FTP
+        if i < 60:
+            res_dict['{0:2d}sec(w)'.format(i)] = max_power
+            res_dict['{0:2d}sec(w/kg)'.format(i)] = max_power / weight
+        else:
+            res_dict['{0:.0f}min(w)'.format(i/60)] = max_power
+            res_dict['{0:.0f}min(w/kg)'.format(i/60)] = max_power/weight
+        if i == 1200:
+            res_dict['EventFTP(w)'] =  max_power * 0.95
+            res_dict['EventFTP(w/kg)'] =  max_power / weight * 0.95
+
+# Create and populate df fields for moving average for each hr interval
+# and calculate maxima
+def hr_interval_stats(df, res_dict):
+    hr_intervals = [15, 30, 60, 300, 1200]
+
+    hr_max = [] # List to store max hr for each time interval
+    for i in hr_intervals:
+        field_name = 'hr_ma' + str(i)
+        interval = str(i) + 's'
+        df[field_name] = df['heart_rate'].rolling(interval, min_periods = i).mean()
+        max_hr = df[field_name].max()
+        hr_max.append(max_hr)
+
+        if i < 60:
+            res_dict['{0:2d}sec(bpm)'.format(i)] = max_hr
+        else:
+            res_dict['{0:.0f}min(bpm)'.format(i/60)] = max_hr
+
+# Plot the data and output to screen if only one rider, output to files
+# if there are 1 or more riders
+def plot_data(df, name, event, no_res):
+    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 6))
+
+    ax[0].plot(df.timestamp, df.heart_rate, label = 'HR')
+    ax[0].set_title('Heart Rate')
+
+    ax[1].plot(df.timestamp, df.power, label = 'Power')
+    ax[1].plot(df.timestamp, df.power_ma1200, label = '20 min moving average')
+    ax[1].plot(df.timestamp, df.power_ma300, label = '5 min moving average')
+    ax[1].plot(df.timestamp, df.power_ma60, label = '1 min moving average')
+    ax[1].set_title('Power')
+
+    locator = mdates.AutoDateLocator(minticks=20, maxticks=30)
+    formatter = mdates.ConciseDateFormatter(locator)
+
+    for a in ax:
+        a.grid(True)
+        a.xaxis.set_major_locator(locator)
+        a.xaxis.set_major_formatter(formatter)
+        a.set_xlabel('Time')
+        a.legend(loc='best')
+        for label in a.get_xticklabels():
+            label.set_rotation(40)
+            label.set_horizontalalignment('right')
+    if no_res == 1:
+        plt.show()
+    plt.savefig('Plot-'+ event + '-' + name + '.png')
+    plt.close()
+
 # Read the each fit file in the folder 'event' and calculate stats
 # Race_distance is used to calculate means ignoring cooldown
 def process_fit_data(event, race_distance):
@@ -71,120 +152,91 @@ def process_fit_data(event, race_distance):
     fit_inputs = glob.glob('.' + fit_folder + '*.fit')
     print(fit_inputs)
 
-
     # Set columns for result data frame so that the .csv sequence is guarenteed.
-    # These need to be the same as those created in the loops below
-    res_cols = ['Name', 'FileName', 'Weight', 'MeanPower(w)', 'MeanPower(w/kg)',
+    # These need to be the same as those created below
+    res_cols = ['Name', 'Weight', 'FTP', 'Distance(km)', 'Duration(min)',
+                'MeanPower(w)', 'NormalisedPower(w)',
+                'MeanPower(w/kg)', 'IntensityFactor', 'TrainingStressScore',
+                'EventFTP(w)', 'EventFTP(w/kg)', 'MaxHR',  'MeanHR', 'MeanCadence',
                 '15sec(w)','15sec(w/kg)','15sec(bpm)',
                 '30sec(w)', '30sec(w/kg)','30sec(bpm)',
                 '1min(w)', '1min(w/kg)', '1min(bpm)',
                 '5min(w)', '5min(w/kg)', '5min(bpm)',
-                '20min(w)', '20min(w/kg)', '20min(bpm)',
-                'FTP(w)', 'FTP(w/kg)', 'MaxHR',  'MeanHR', 'MeanCadence']
+                '20min(w)', '20min(w/kg)', '20min(bpm)', 'FileName']
     res_df = pd.DataFrame(columns = res_cols)
-
-    # Set intervals, in seconds, for which moving averagaes are calculated
-    power_intervals = [15, 30, 60, 300, 1200]
-    hr_intervals = [15, 30, 60, 300, 1200]
-
 
     # Loop through all the files in the fit folder
     for fit_input in fit_inputs:
 
         # Filename needs to start with weight in kg followed by '-'
-        weight = float(os.path.basename(fit_input).split('-')[0])
+        name = os.path.basename(fit_input).split('_')[0]
+        weight = float(os.path.basename(fit_input).split('_')[1])
+        FTP = float(os.path.basename(fit_input).split('_')[2])
+
+        res_df.FileName.str.split('-').str[1]
 
         #  Read fit file using the function defined above
         data = read_fit_file(fit_input, allowed_fields, required_fields)
 
-        # Convert list to a data frame
+        # Convert list to a data frame and sort on timestamp
         df = pd.DataFrame(data)
-
-        # Calculate moving average of power over 20 minute rolling window and
-        # moving average to a new column in the data frame
         df.index = df.timestamp
         df = df.sort_index()
 
-
         # Print / save identifying info
         print('--- ', os.path.basename(fit_input), ' ---')
-        print('Weight {0:0.1f} kg'.format(weight))
-        res_dict = {'FileName': os.path.basename(fit_input), 'Weight': weight}
+        res_dict = {'FileName': os.path.basename(fit_input), 'Weight': weight, 'Name': name, 'FTP': FTP}
 
-        # Create and populate df fields for moving average for each power interval
-        # and calculate maxima
-        power_max = [] # List to store max power for each time interval
-        for i in power_intervals:
-            field_name = 'power_ma' + str(i)
-            interval = str(i) + 's'
-            df[field_name] = df['power'].rolling(interval, min_periods = i).mean()
-            power_max.append(df[field_name].max())
-
-        # Create and populate df fields for moving average for each hr interval
-        # and calculate maxima
-        hr_max = [] # List to store max hr for each time interval
-        for i in hr_intervals:
-            field_name = 'hr_ma' + str(i)
-            interval = str(i) + 's'
-            df[field_name] = df['heart_rate'].rolling(interval, min_periods = i).mean()
-            hr_max.append(df[field_name].max())
-
-        # Prints power moving average max for each interval specified in the
-        # list power_intervals[]
-        print('Max Power over:')
-        for i in range(len(power_intervals)):
-            if power_intervals[i] < 60:
-                print('{0:2d} sec: {1:0.0f} watt, {2:0.2f} w/kg'
-                      .format(power_intervals[i], power_max[i], power_max[i]/weight))
-                res_dict['{0:2d}sec(w)'.format(power_intervals[i])] = power_max[i]
-                res_dict['{0:2d}sec(w/kg)'.format(power_intervals[i])] = power_max[i]/weight
-            else:
-                print('{0:2.0f} min: {1:0.0f} watt, {2:0.2f} w/kg'
-                      .format(power_intervals[i]/60, power_max[i], power_max[i]/weight))
-                res_dict['{0:.0f}min(w)'.format(power_intervals[i]/60)] = power_max[i]
-                res_dict['{0:.0f}min(w/kg)'.format(power_intervals[i]/60)] = power_max[i]/weight
-            if power_intervals[i] == 1200:
-                print('FTP {0:0.0f}w, {1:0.2f}w/kg'.format(power_max[i] * 0.95, power_max[i]/weight * 0.95))
-                res_dict['FTP(w)'] = power_max[i] * 0.95
-                res_dict['FTP(w/kg)'] =  power_max[i]/weight * 0.95
-
-        # Prints heart rate moving average max for each interval specified in the
-        # list hr_intervals[]
-        print('Max Heart Rate over:')
-        for i in range(len(hr_intervals)):
-            if hr_intervals[i] < 60:
-                print('{0:2d} sec: {1:0.0f} bpm'
-                      .format(hr_intervals[i], hr_max[i]))
-                res_dict['{0:2d}sec(bpm)'.format(hr_intervals[i])] = hr_max[i]
-            else:
-                print('{0:2.0f} min: {1:0.0f} bpm'
-                      .format(hr_intervals[i]/60, hr_max[i]))
-                res_dict['{0:.0f}min(bpm)'.format(power_intervals[i]/60)] = power_max[i]
+        # Calculate interval stats and append to results (res_dict)
+        power_interval_stats(df, weight, res_dict)
+        hr_interval_stats(df, res_dict)
 
         # Print mean power and HR for a subset of the activity
         # Start at first distance > 0.01 (allow 10m movement) up to race_distance to cater for start delays
         dfr = df[(df.power != float('nan')) & (df.distance > 0.01) & (df.distance <= race_distance)]
 
-        print('\nMean power: {0:0.0f}w, {1:0.2f}w/kg, Mean HR: {2:0.0f}, Max HR: {3:0.0f}, Mean cadence: {4:0.0f}'
-              .format(dfr.power.mean(), dfr.power.mean()/weight, dfr.heart_rate.mean(), dfr.heart_rate.max(),
-              dfr.cadence.mean()))
-        print(' ')
+        res_dict['Distance(km)'] = race_distance
         res_dict['MeanPower(w)'] = dfr.power.mean()
         res_dict['MeanPower(w/kg)'] = dfr.power.mean()/weight
         res_dict['MeanHR'] = dfr.heart_rate.mean()
         res_dict['MaxHR'] = dfr.heart_rate.max()
         res_dict['MeanCadence'] = dfr.cadence.mean()
 
+        # Creates subset that excludes rows with no 30s moving power average and create col with ^4
+        dfrNP = dfr[(dfr.power_ma30 > 0)].copy()
+        dfrNP['power_ma30_4'] = dfrNP['power_ma30']**4
+
+        # Calculate Normalised Power (this requires 30s interval to have been calculated above)
+        # Calculate NP, IF and TSS
+        duration = pd.Timedelta(dfr.timestamp.max() - dfr.timestamp.min()).seconds
+        res_dict['Duration(min)'] = duration / 60
+        normalised_power = (dfrNP.power_ma30_4.mean())**0.25 # Nominal Power
+        res_dict['NormalisedPower(w)'] = normalised_power
+
+        # If FTP was specified calculate IF and TSS
+        if FTP > 0:
+            intensity_factor = normalised_power / FTP  # Intensity Factor
+            res_dict['IntensityFactor'] = intensity_factor
+
+            training_stress_score = (duration * normalised_power * intensity_factor) / (FTP * 36)
+            res_dict['TrainingStressScore'] = training_stress_score
+
+        # Pretty print results sictionary to stdout
+        pp.pprint(res_dict)
+
+        # Append results dictionary to dataframe
         res_df = res_df.append(res_dict, ignore_index=True)
 
-    # Strip name out from file name and add as a new column
-    res_df['Name'] = res_df.FileName.str.split('-').str[1]
+        # Plot data
+        plot_data(df, name, event, len(fit_inputs))
+
     # Write resulta data frame to CSV
     res_df.to_csv('Results-' + event + '.csv', float_format='%.2f', index=False)
     print('Done')
 
 
 # UI that prompts user to select folder
+# -------------------------------------
 
 # Function that is called when OK is pressed
 # Call main function with folder name and the distance to be used
@@ -224,6 +276,7 @@ Label(mainframe, text="Choose an event").grid(row = 1, column = 1)
 variable = StringVar(root)
 variable.set(subfolders[0]) # default value
 
+# Menu that shows a list of subfolders
 menu = OptionMenu(mainframe, variable, *subfolders)
 menu.grid(row = 2, column =1)
 
